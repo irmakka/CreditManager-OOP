2e257534e0aba8775bf48dbef102ae1b571e5f77
Irmak Kahya <138292432+irmakka@users.noreply.github.com>
2024-12-11 18:11:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BaseCreditManager  creditManager=new StudentCreditManager();
-        BaseCreditManager creditManager1=new TeacherCreditManager();
-         System.out.println(creditManager.creditCalculate(1000));
-        System.out.print( creditManager1.creditCalculate(1000));
-        
+        BaseCreditManager[] baseCreditManager= new BaseCreditManager[]{
+            new StudentCreditManager(),new TeacherCreditManager()
+        };
+        for(BaseCreditManager creditManager:baseCreditManager){
+            System.out.println(creditManager.creditCalculate(1000.0));
+        }
 
     }
 }
